aedacdaa3225f31b9b921880eb9660137ba78b1a
yate <mwyeatts@gmail.com>
2015-09-30 21:26:03 -0400

    format

--- src/main/java/powerpolicy/PowerPolicyApplication.java
+++ src/main/java/powerpolicy/PowerPolicyApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PowerPolicyApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PowerPolicyApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(PowerPolicyApplication.class, args);
+	}
 }
